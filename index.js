bf817a0089ddf8822d56384e80576aeff3613d09
Michelle <53919326+colombomf@users.noreply.github.com>
2023-01-21 00:44:30 -0300

    fix alt text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* How to add a title | {} is a placeholder for props */
 const h2 = React.createElement('h2', {},'"Is it me you\'re looking for?"');
 /* How to add an image */
-const img = React.createElement('img', {src: './lr.jpg', alt:'Lionel Richie  drawing with Hello written'});
+const img = React.createElement('img', {src: './lr.jpg', alt:'Lionel Richie drawing with Hello written over it'});
 /* Creates a container for Title + Image + CSS */
 const element = React.createElement('div', {className: "center-div"}, img, h2);
 /* Renders the div to the DOM */
